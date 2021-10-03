cc60f673715e4a13aa1a4d4838c33c606e461a73
Aibulat Urazov <aibulat.urazov@icloud.com>
2021-10-03 20:58:21 +0500

    commit

--- src/js/TabMenu.js
+++ src/js/TabMenu.js
@@ -1,7 +1,13 @@
 const TabMenu = () => {
     let href = window.location.href
+    let link = document.querySelectorAll('.header__nav-link')
 
-    console.log(href);
+    link.forEach(el => {
+        let link2 = el.dataset.menu
+        if (href === link2) {
+            el.classList.add('active')
+        }
+    })
 }
 
 export default TabMenu
